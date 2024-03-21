dfa2235be32b6fcc142a6d7034a92d53f8f19a05
John <jun0205@live.com>
2024-03-21 13:33:14 +0800

    Update styles

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -9,12 +9,12 @@ export interface EmptyScreenProps extends Pick<UseChatHelpers, 'setInput'> {
 export function EmptyScreen({ provider, setInput }: EmptyScreenProps) {
   return (
     <div className="h-full mx-auto max-w-4xl px-4 flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center border rounded-full p-2 mb-4">
-        {provider === 'openai' && <IconOpenAI className="size-8" />}
-        {provider === 'google' && <IconGoogleAI className="size-8" />}
-        {provider === 'anthropic' && <IconClaudeAI className="size-8" />}
+      <div className="flex items-center justify-center border rounded-full mb-4">
+        {provider === 'openai' && <IconOpenAI className="size-7 m-2.5" />}
+        {provider === 'google' && <IconGoogleAI className="size-7 m-2.5" />}
+        {provider === 'anthropic' && <IconClaudeAI className="size-7 m-2.5" />}
       </div>
-      <div className="mb-10 text-2xl font-medium">
+      <div className="mb-10 text-lg lg:text-2xl font-medium">
         How can I help you today?
       </div>
     </div>
