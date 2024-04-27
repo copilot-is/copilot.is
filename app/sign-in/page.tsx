561e7cc1d8dfd19e00bf33feb7ba653a45f6950e
John <jun0205@live.com>
2024-04-27 23:18:01 +0800

    Update login page UI

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -15,12 +15,23 @@ export default async function SignInPage() {
     redirect('/')
   }
 
-  const githubEnabled = process.env.AUTH_GITHUB_ENABLED === 'true';
-  const googleEnabled = process.env.AUTH_GOOGLE_ENABLED === 'true';
+  const githubEnabled = process.env.AUTH_GITHUB_ENABLED === 'true'
+  const googleEnabled = process.env.AUTH_GOOGLE_ENABLED === 'true'
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <LoginButton githubEnabled={githubEnabled} googleEnabled={googleEnabled} />
+    <div className="flex size-full items-center justify-center">
+      <div className="bg-background rounded-lg border p-8 shadow-md">
+        <div className="space-y-6">
+          <h1 className="px-3 text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Sign in to your account
+          </h1>
+
+          <LoginButton
+            githubEnabled={githubEnabled}
+            googleEnabled={googleEnabled}
+          />
+        </div>
+      </div>
     </div>
   )
 }
--- components/login-button.tsx
+++ components/login-button.tsx
@@ -2,13 +2,16 @@
 
 import * as React from 'react'
 import { signIn } from 'next-auth/react'
+
+import { cn } from '@/lib/utils'
 import { Button, type ButtonProps } from '@/components/ui/button'
 import { IconGitHub, IconGoogle, IconSpinner } from '@/components/ui/icons'
 
-interface LoginButtonProps extends Omit<ButtonProps, 'children' | 'disabled' | 'onClick'> {
-  githubEnabled?: boolean;
-  googleEnabled?: boolean;
-  className?: string;
+interface LoginButtonProps
+  extends Omit<ButtonProps, 'children' | 'disabled' | 'onClick'> {
+  githubEnabled?: boolean
+  googleEnabled?: boolean
+  className?: string
 }
 
 export function LoginButton({
@@ -17,39 +20,52 @@ export function LoginButton({
   className,
   ...buttonProps
 }: LoginButtonProps) {
-  const [loadingProvider, setLoadingProvider] = React.useState<string | null>(null);
+  const [loadingProvider, setLoadingProvider] = React.useState<string | null>(
+    null
+  )
 
   const handleSignIn = (provider: string) => {
-    setLoadingProvider(provider);
-    signIn(provider, { callbackUrl: '/' });
-  };
+    setLoadingProvider(provider)
+    signIn(provider, { callbackUrl: '/' })
+  }
 
   return (
-    <div className={`${className} flex flex-col space-y-2`}>
+    <div className={cn('flex flex-col space-y-4', className)}>
       {githubEnabled && (
         <Button
           {...buttonProps}
           variant="outline"
+          size="lg"
           onClick={() => handleSignIn('github')}
           disabled={loadingProvider !== null}
-          className="w-full"
+          className="w-full shadow-none"
         >
-          {loadingProvider === 'github' ? <IconSpinner className="animate-spin" /> : <IconGitHub />}
+          {loadingProvider === 'github' ? (
+            <IconSpinner className="size-5 animate-spin" />
+          ) : (
+            <IconGitHub className="size-5" />
+          )}
           <span className="ml-2">Login with GitHub</span>
         </Button>
       )}
+
       {googleEnabled && (
         <Button
           {...buttonProps}
           variant="outline"
+          size="lg"
           onClick={() => handleSignIn('google')}
           disabled={loadingProvider !== null}
-          className="w-full"
+          className="w-full shadow-none"
         >
-          {loadingProvider === 'google' ? <IconSpinner className="animate-spin" /> : <IconGoogle />}
+          {loadingProvider === 'google' ? (
+            <IconSpinner className="size-5 animate-spin" />
+          ) : (
+            <IconGoogle className="size-5" />
+          )}
           <span className="ml-2">Login with Google</span>
         </Button>
       )}
     </div>
-  );
+  )
 }
